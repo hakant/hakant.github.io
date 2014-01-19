34c08d90ff8cc031536d7f0015da1ff109a55bcf
hakant <hakantuncer@gmail.com>
2014-01-19 08:50:52 +0100

    pygments are enabled

--- plugins/pygments_code.rb
+++ plugins/pygments_code.rb
@@ -2,17 +2,19 @@ require 'pygments'
 require 'fileutils'
 require 'digest/md5'
 
-PYGMENTS_CACHE_DIR = File.expand_path('../../.pygments-cache', __FILE__)
+PYGMENTS_CACHE_DIR = File.expand_path('../../_code_cache', __FILE__)
 FileUtils.mkdir_p(PYGMENTS_CACHE_DIR)
 
 module HighlightCode
   def highlight(str, lang)
-    lang = 'ruby' if lang == 'ru'
-    lang = 'objc' if lang == 'm'
-    lang = 'perl' if lang == 'pl'
-    lang = 'yaml' if lang == 'yml'
-    str = pygments(str, lang).match(/<pre>(.+)<\/pre>/m)[1].to_s.gsub(/ *$/, '') #strip out divs <div class="highlight">
-    tableize_code(str, lang)
+    str = pygments(str, lang).match(/<pre>(.+)<\/pre>/m)[1].to_s.gsub(/\s*$/, '') #strip out divs <div class="highlight">
+    table = '<div class="highlight"><table cellpadding="0" cellspacing="0"><tr><td class="gutter"><pre class="line-numbers">'
+    code = ''
+    str.lines.each_with_index do |line,index|
+      table += "<span class='line'>#{index+1}</span>\n"
+      code  += "<div class='line'>#{line}</div>"
+    end
+    table += "</pre></td><td class='code' width='100%'><pre><code class='#{lang}'>#{code}</code></pre></td></tr></table></div>"
   end
 
   def pygments(code, lang)
@@ -21,25 +23,12 @@ module HighlightCode
       if File.exist?(path)
         highlighted_code = File.read(path)
       else
-        begin
-          highlighted_code = Pygments.highlight(code, :lexer => lang, :formatter => 'html', :options => {:encoding => 'utf-8', :startinline => true})
-        rescue MentosError
-          raise "Pygments can't parse unknown language: #{lang}."
-        end
+        highlighted_code = Pygments.highlight(code, :lexer => lang, :formatter => 'html')
         File.open(path, 'w') {|f| f.print(highlighted_code) }
       end
     else
-      highlighted_code = Pygments.highlight(code, :lexer => lang, :formatter => 'html', :options => {:encoding => 'utf-8', :startinline => true})
+      highlighted_code = Pygments.highlight(code, :lexer => lang, :formatter => 'html')
     end
     highlighted_code
-  end
-  def tableize_code (str, lang = '')
-    table = '<div class="highlight"><table><tr><td class="gutter"><pre class="line-numbers">'
-    code = ''
-    str.lines.each_with_index do |line,index|
-      table += "<span class='line-number'>#{index+1}</span>\n"
-      code  += "<span class='line'>#{line}</span>"
-    end
-    table += "</pre></td><td class='code'><pre><code class='#{lang}'>#{code}</code></pre></td></tr></table></div>"
   end
 end
